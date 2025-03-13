da4e250cef3b18b650bb4eab949c5084b8b9c86c
Gabriel <gabrielfidelibus@gmail.com>
2025-03-13 20:09:59 +0100

    v1 deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://tu-usuario.github.io',
+  base: '/nombre-del-repositorio',
 });
